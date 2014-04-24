b93e86b439d7eddb452e9c7bbf60b5add520e606
melechever <melechever@gmail.com>
2014-04-23 21:58:44 -0430

    Added solution
    Added solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Author: Melany Echeverria
+## Create the special matrix to compute the inverse of a square matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i<<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) i <<- solve
+  getsolve <- function() i
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Compute the inverse of a square matrix and saves it in cache the first time
+## If the result is already in cache, return the saved data
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getsolve()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  ## End.process shows the time elapsed calculating the inverse
+  ## This is to show the time we save using this experiment
+  Start.process <- Sys.time()
+  i <- solve(data, ...)
+  End.process <- Sys.time()
+  Exec.time <- End.process - Start.process
+  print(Exec.time)
+  x$setsolve(i)
+  i
 }
